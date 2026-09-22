5b8b8ed609a4ed2900f3ba0b333266d1bd7d5fc7
Akshay RR <akshay.rr@growmax.io>
2026-09-22 23:39:40 +0530

    refactor: convert multi-page portfolio to single-page scrolling layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
 import Hero from '../components/sections/Hero';
+import AboutSection from '../components/sections/About';
+import SkillsSection from '../components/sections/Skills';
+import ExperienceSection from '../components/sections/Experience';
+import ProjectsSection from '../components/sections/Projects';
+import CertificationsSection from '../components/sections/Certifications';
+import ContactSection from '../components/sections/Contact';
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
+    <div className="w-full flex flex-col">
+      <div id="home">
+        <Hero />
+      </div>
+      <div id="about">
+        <AboutSection />
+      </div>
+      <div id="skills">
+        <SkillsSection />
+      </div>
+      <div id="experience">
+        <ExperienceSection />
+      </div>
+      <div id="projects">
+        <ProjectsSection />
+      </div>
+      <div id="certifications">
+        <CertificationsSection />
+      </div>
+      <div id="contact">
+        <ContactSection />
+      </div>
     </div>
   );
 };
